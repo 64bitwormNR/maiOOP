e90f60ffb224a012e40d48f7150acb64af464ff2
64bitwormNR <74878722+64bitwormNR@users.noreply.github.com>
2021-11-06 19:17:46 +0500

    Add files via upload

---
+++ lab5/square.cpp
@@ -0,0 +1,66 @@
+//SQUARE.CPP
+#include "square.h"
+
+#include <cmath>
+
+Square::Square() {}
+
+Square::~Square() {printf("destroyed\n");}
+
+Square::Square(std::istream &is)
+{
+	printf("created\n");
+	is >> *this;
+}
+
+Square::Square(Point pnt1, Point pnt2, Point pnt3, Point pnt4)
+{
+	p1 = pnt1;
+	p2 = pnt2;
+	p3 = pnt3;
+	p4 = pnt4;
+}
+
+size_t Square::VertexesNumber()
+{
+	return 4;
+}
+
+double Square::Area()
+{
+	return calcTriangleArea(p1,p2,p3)+calcTriangleArea(p3,p4,p1);
+}
+
+void Square::Print(std::ostream& os)
+{
+	os << *this;
+}
+
+void Square::Read(std::istream& is)
+{
+	is >> *this;
+}
+
+Square& Square::operator=(const Square& sq)
+{
+	p1=sq.p1;
+	p2=sq.p2;
+	p3=sq.p3;
+	p4=sq.p4;
+	return *this;
+}
+
+bool Square::operator==(const Square& sq)
+{
+	return p1==sq.p1 && p2==sq.p2 && p3==sq.p3 && p4==sq.p4;
+}
+
+std::istream& operator>>(std::istream& is, Square& r) {
+  is >> r.p1 >> r.p2 >> r.p3 >> r.p4;
+  return is;
+}
+
+std::ostream& operator<<(std::ostream& os, Square& r) {
+  os << "Square: " << r.p1 << " " << r.p2 << " " << r.p3 << " " << r.p4;
+  return os;
+}
